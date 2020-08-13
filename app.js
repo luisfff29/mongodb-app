66dc84175e2fc7849c785ce462c041af19eca7c8
luisfff29 <luisfff29@gmail.com>
2020-08-13 10:03:03 -0500

    Insert a Document

--- app.js
+++ app.js
@@ -7,6 +7,22 @@ const url = "mongodb://localhost:27017";
 // Database Name
 const dbName = "myproject";
 
+const insertDocuments = function (db, callback) {
+    // Get the documents collection
+    const collection = db.collection("documents");
+    // Insert some documents
+    collection.insertMany([{ a: 1 }, { a: 2 }, { a: 3 }], function (
+        err,
+        result
+    ) {
+        assert.equal(err, null);
+        assert.equal(3, result.result.n);
+        assert.equal(3, result.ops.length);
+        console.log("Inserted 3 documents into the collection");
+        callback(result);
+    });
+};
+
 // Use connect method to connect to the server
 MongoClient.connect(url, (err, client) => {
     assert.equal(null, err);
@@ -14,5 +30,7 @@ MongoClient.connect(url, (err, client) => {
 
     const db = client.db(dbName);
 
-    client.close();
+    insertDocuments(db, function () {
+        client.close();
+    });
 });
